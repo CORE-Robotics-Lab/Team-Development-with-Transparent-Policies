78860bff799cecd7c3d32c357f6187e56395833c
Michael Munje <mmunje3@gatech.edu>
2023-02-01 12:02:33 -0500

    Add BC recorder.

---
+++ ipm/bin/overcooked_recorder.py
@@ -0,0 +1,159 @@
+import argparse
+import pandas as pd
+import pygame
+from overcooked_ai.src.overcooked_ai_py.visualization.state_visualizer import StateVisualizer
+from ipm.overcooked.overcooked import OvercookedPlayWithFixedPartner, OvercookedSelfPlayEnv
+
+
+class OvercookedGameRecorder:
+    def __init__(self, traj_filepath, layout_name='forced_coordination_tomato', n_episodes=1, SCREEN_WIDTH=1600, SCREEN_HEIGHT=900):
+        self.SCREEN_WIDTH = SCREEN_WIDTH
+        self.SCREEN_HEIGHT = SCREEN_HEIGHT
+        self.layout_name = layout_name
+        self.traj_filepath = traj_filepath
+        self.n_episodes = n_episodes
+
+        self.n_actions = 13 # hardcoded for now
+        self.actions = list(range(self.n_actions))
+
+        num_primitives = 6
+        num_skills = self.n_actions - num_primitives
+        # skills
+        self.skills_to_idx = {str(i):num_primitives + i for i in range(num_skills)}
+        # 1 -> GET CLOSEST ONION
+        # 2 -> GET CLOSEST TOMATO
+        # 3 -> GET CLOSEST DISH
+        # 4 -> GET CLOSEST SOUP
+        # 5 -> SERVE SOUP
+        # 6 -> BRING TO CLOSEST POT
+        # 7 -> PLACE ON CLOSEST COUNTER
+
+        self.ego_idx = 0
+        self.alt_idx = (self.ego_idx + 1) % 2
+
+        # other_agent = OtherAgentWrapper(possible_commands=self.actions,
+        #                                 skills_to_idx=self.skills_to_idx,
+        #                                 get_action_fn=self.get_human_action,
+        #                                 alt_idx=self.alt_idx)
+
+        # self.env = OvercookedPlayWithFixedPartner(partner=other_agent, layout_name=layout_name, ego_idx=self.ego_idx,
+        #                                          reduced_state_space_ego=False,
+        #                                          reduced_state_space_alt=False)
+
+        self.env = OvercookedSelfPlayEnv(layout_name=layout_name, ego_idx=self.ego_idx,
+                                         reduced_state_space_ego=False,
+                                         reduced_state_space_alt=False)
+
+        assert self.n_actions == self.env.n_actions_ego
+        assert self.env.n_actions_ego == self.env.n_actions_alt
+
+        self.states = []
+        self.actions = []
+        self.episode_idxs = []
+        self.agent_idxs = []
+
+        self.current_episode_num = 0
+
+        self.visualizer = StateVisualizer()
+        pygame.init()
+        self.screen = pygame.display.set_mode((self.SCREEN_WIDTH, self.SCREEN_HEIGHT))
+
+    def get_human_action(self, agent_idx):
+        # force the user to make a move
+
+        # KEY -> ACTION
+
+        # LEFT -> LEFT
+        # UP -> UP
+        # DOWN -> DOWN
+        # RIGHT -> RIGHT
+        # SPACE -> INTERACT
+
+        # 1 -> GET CLOSEST ONION
+        # 2 -> GET CLOSEST TOMATO
+        # 3 -> GET CLOSEST DISH
+        # 4 -> GET CLOSEST SOUP
+        # 5 -> SERVE SOUP
+        # 6 -> BRING TO CLOSEST POT
+        # 7 -> PLACE ON CLOSEST COUNTER
+
+        # 0 -> STAND STILL
+
+        self.visualize_state(self.env.state)
+
+        agent_str = 'first' if agent_idx == 0 else 'second'
+        color = 'blue' if agent_idx == 0 else 'green'
+        print(f'Please enter the action to take for {agent_str} agent (hat color: {color})')
+
+        command = None
+        while command is None:
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LEFT:
+                        command = 3
+                    elif event.key == pygame.K_UP:
+                        command = 0
+                    elif event.key == pygame.K_DOWN:
+                        command = 1
+                    elif event.key == pygame.K_RIGHT:
+                        command = 2
+                    elif event.key == pygame.K_SPACE:
+                        command = 5
+                    # otherwise check for key 0
+                    elif event.key == pygame.KSCAN_0:
+                        command = 4
+                    else:
+                        # get the unicode
+                        skill_idx = event.unicode
+                        assert command in self.skills_to_idx
+                        command = self.skills_to_idx[skill_idx]
+
+        self.states.append(self.env.state)
+        self.actions.append(command)
+        self.episode_idxs.append(self.current_episode_num)
+        self.agent_idxs.append(agent_idx)
+
+        return command
+
+
+    def visualize_state(self, state):
+        self.screen.fill((0, 0, 0))
+        state_visualized_surf = self.visualizer.render_state(state=state, grid=self.env.base_env.mdp.terrain_mtx)
+        self.screen.blit(pygame.transform.scale(state_visualized_surf, (self.SCREEN_WIDTH, self.SCREEN_HEIGHT)), (0, 0))
+        pygame.display.flip()
+
+    def record_trajectories(self):
+        done = False
+        total_reward = 0
+
+        # data format:
+        # [state, action, episode, agent_idx]
+
+        for i in range(self.n_episodes):
+            self.env.reset()
+            clock = pygame.time.Clock()
+            # self.visualize_state(self.env.state)
+            clock.tick(60)
+            agent_idx = 0
+
+            while not done:
+                action = self.get_human_action(agent_idx=agent_idx)
+                _, reward, done, info = self.env.step(action)
+                agent_idx = (agent_idx + 1) % 2
+                total_reward += reward
+                clock.tick(60)
+
+            self.current_episode_num += 1
+
+        df = pd.DataFrame({'state': self.states, 'action': self.actions, 'episode': self.episode_idxs, 'agent_idx': self.agent_idxs})
+        df.to_csv(self.traj_filepath, index=False)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Records trajectories of human playing overcooked')
+    parser.add_argument('--traj_filepath', help='the output file to save the data to', type=str)
+    parser.add_argument('--layout_name', help='the layout to use', type=str, default='forced_coordination_tomato')
+    parser.add_argument('--n_episodes', help='the number of episodes to record', type=int, default=1)
+    args = parser.parse_args()
+
+    demo = OvercookedGameRecorder(args.traj_filepath, layout_name=args.layout_name, n_episodes=args.n_episodes)
+    demo.record_trajectories()
